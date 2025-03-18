0a616be333d439c81b83fdcade198831bb3114d7
Theo_Helmikstol <theohelmikstol@gmail.com>
2025-03-18 13:25:58 +0100

    test

--- php/mainNav.php
+++ php/mainNav.php
@@ -1,6 +1,6 @@
 <nav>
   <a href="indexSiden.php">Hjem</a>
-  <a href="#">Orale steroider</a>
+  <a href="oSteroider.html">Orale steroider</a>
   <a href="#">Injiserbare steroider</a> 
   <a href="#">PCT</a>
 </nav>
